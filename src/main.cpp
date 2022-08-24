31ce1a991d76ca87bb88b007f4577b9f19ee0479
Aleksander Auflem <Aleksander.Auflem@gmail.com>
2022-08-24 15:57:44 +0200

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define LED_Y 5
 #define LED_R 6
 #define StartPushButton 7
-#define ResetPushButton 8
+
 unsigned long currentTime;
 unsigned long refTime;
 unsigned long printTime;
@@ -101,6 +101,9 @@ void LED_bleeding_Stopped()
   LED_R_OFF();
 }
 
+
+// Function for printing pressure value every 0.25 sec
+
 void PrintVal() {
   if (millis() - printTime > 250)
   {
@@ -116,19 +119,25 @@ void setup() {
   pinMode(LED_Y, OUTPUT);
   pinMode(LED_R, OUTPUT);
   pinMode(StartPushButton, INPUT_PULLUP);
-  pinMode(ResetPushButton, INPUT_PULLUP);
+
+  //timestamps
   currentTime = millis();
   printTime = millis();
+
   STATE current_state{INIT};
+
   // open I2C communication
   Wire.begin();
   Serial.println("Ready");
   pinMode(Pump, OUTPUT);
+
+  //Changing the frequency to remove high pitch sound from the pump
   TCB1.CTRLA &= ~TCB_ENABLE_bm;// Turn off timer while we change parameters.
   TCB1.CTRLA &= ~TCB_CLKSEL_gm;// Clear all CLKSEL bits.
   TCB1.CTRLA |= TCB_CLKSEL_CLKDIV1_gc;// Set prescaler to 2.
   TCB1.CTRLA |= TCB_ENABLE_bm;// Re-enable timer. Pins 5 and 9 now run at 31.25 kHz
-
+  
+  // PWM signal to run the pump
   analogWrite(Pump, 43);
 }
 
@@ -136,16 +145,17 @@ void setup() {
 
 void loop() {
   // update sensor reading
-  analogWrite(Pump, 43);
   abp.update();
+  analogWrite(Pump, 43);
   PrintVal();
   
+  //State switching
   switch(current_state)
   {
     case (INIT):
     analogWrite(Pump, 0);
     Serial.println("Start?");
-    //if (abp.pressure() < Pressure_limit)
+    //Criteria for starting the bleeding
     if (digitalRead(StartPushButton) == false)
     {
       current_state = High_flow;
@@ -160,7 +170,6 @@ void loop() {
 
       if(millis() - low_time >= coag_time)
       {
-        Serial.println(abp.pressure());
         LED_bleeding_Stopped();
         analogWrite(Pump, 0);
         while(true);
